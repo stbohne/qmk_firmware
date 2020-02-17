02f4f36db4e5d04882ad18cc4a39b7bb0dfdaf57
Stefan Bohne <stbohne@gmail.com>
2020-02-17 20:06:23 +0900

    keymap cleanup

--- keyboards/keebio/viterbi/keymaps/stbohne/keymap.c
+++ keyboards/keebio/viterbi/keymaps/stbohne/keymap.c
@@ -33,9 +33,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
      DEL ,BSPC, A  , R  , S  , T  , G  ,      K  , N  , E  , I  , O  ,QUOT,BSLS,
   //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
-         ,LSFT, Z  , X  , C  , D  , V  ,      M  , H  ,COMM,DOT ,SLSH,RSFT,LEAD,
+         ,LSFT, Z  , X  , C  , D  , V  ,      M  , H  ,COMM,DOT ,SLSH,RSFT,    ,
   //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
-      SH ,LCTL,FN1 ,LGUI,LALT,SPC ,SPC ,     ENT ,ENT ,RALT,APP ,FN1 ,RCTL, SH 
+     HYPR,LCTL,FN1 ,LGUI,LALT,SPC ,SPC ,     ENT ,ENT ,RALT,APP ,FN1 ,RCTL,HYPR
   //`----+----+----+----+----+----+----'    `----+----+----+----+----+----+----'
   ),
 
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
          ,LSFT, Z  , X  , C  , V  , B  ,      N  , M  ,COMM,DOT ,SLSH,RSFT,LEAD,
   //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
-      SH ,LCTL,FN1 ,LGUI,LALT,SPC ,SPC ,     ENT ,ENT ,RALT,APP ,FN1 ,RCTL, SH 
+     HYPR,LCTL,FN1 ,LGUI,LALT,SPC ,SPC ,     ENT ,ENT ,RALT,APP ,FN1 ,RCTL,HYPR
   //`----+----+----+----+----+----+----'    `----+----+----+----+----+----+----'
   ),
 
@@ -57,13 +57,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,----+----+----+----+----+----+----.    ,----+----+----+----+----+----+----.
      PWR ,SLEP, F1 , F2 , F3 , F4 , F5 ,      F6 , F7 , F8 , F9 ,F10 ,F11 ,F12 ,
   //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
-     COLE,    ,    ,BTN2,MS_U,BTN1,VOLU,     PGUP,HOME, UP ,END ,WSCH,    ,    ,
+     COLE,    ,    ,BTN2,MS_U,BTN1,WH_U,     PGUP,HOME, UP ,END ,WSCH,    ,    ,
   //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
-     QWER,    ,BTN3,MS_L,MS_D,MS_R,VOLD,     PGDN,LEFT,DOWN,RGHT,INS ,CALC,    ,
+     QWER,    ,BTN3,MS_L,MS_D,MS_R,WH_D,     PGDN,LEFT,DOWN,RGHT,INS ,CALC,    ,
   //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
-         ,LSFT,    ,    ,    ,    ,MUTE,         ,ACL2,ACL1,ACL0,    ,RSFT,    ,
+         ,LSFT,    ,MUTE,VOLD,VOLU,    ,         ,ACL2,ACL1,ACL0,    ,RSFT,    ,
   //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
-     HYPR,LCTL,FN1X,LGUI,LALT,    ,    ,         ,    ,RALT,APP ,FN1X,RCTL,HYPR
+      SH ,LCTL,FN1X,LGUI,LALT,    ,    ,         ,    ,RALT,APP ,FN1X,RCTL, SH 
   //`----+----+----+----+----+----+----'    `----+----+----+----+----+----+----'
   ),
 };
@@ -80,48 +80,3 @@ const keypos_t hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
   {{0, 3}, {1, 3}, {2, 3}, {3, 3}, {4, 3}, {5, 3}, {6, 3}},
   {{0, 4}, {1, 4}, {2, 4}, {3, 4}, {4, 4}, {5, 4}, {6, 4}},
 };
-
-LEADER_EXTERNS();
-
-static int16_t center_x = -6;
-static int16_t center_y = -10;
-
-void matrix_init_user(void) {
-  //center_x = analogRead(4);
-  //center_y = analogRead(2);
-}
-void matrix_scan_user(void) {
-  LEADER_DICTIONARY() {
-    leading = false;
-    leader_end();
-
-    SEQ_FIVE_KEYS(KC_U, KC_C, KC_W, KC_I, KC_N) {
-      set_unicode_input_mode(UC_WIN);
-    }
-    SEQ_FIVE_KEYS(KC_U, KC_C, KC_L, KC_N, KC_X) {
-      set_unicode_input_mode(UC_WIN);
-    }
-    SEQ_TWO_KEYS(KC_M, KC_U) {
-      send_unicode_hex_string("03BC");
-    }
-    SEQ_FIVE_KEYS(KC_A, KC_L, KC_P, KC_H, KC_A) {
-      send_unicode_hex_string("03B1");
-    }
-  }
-
-  if (0) {
-	  report_mouse_t rm = pointing_device_get_report();
-	  int16_t x = analogRead(4) - center_x - 512;
-	  int16_t y = analogRead(2) - center_y - 512;
-	  x = (int32_t)x * 127l / (512l - (center_x * (x >= 0 ? 1 : -1)));
-	  y = (int32_t)y * 127l / (512l - (center_y * (y >= 0 ? 1 : -1)));
-	  if (isLeftHand) {
-		rm.h = -x / 128;
-		rm.v = y / 128;
-	  } else {
-		rm.x = -x / 16;
-		rm.y = y / 16;
-	  }
-	  pointing_device_set_report(rm);
-  }
-}
